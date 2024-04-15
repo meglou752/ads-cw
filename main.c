ac510afd9ae5e7b985b491ca59c8be031d14de5e
meglou782 <meganmcleod@Megans-MacBook-Pro-4.local>
2024-04-15 12:00:47 +0100

    Adding timestamps

--- main.c
+++ main.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 #include "sudoku.h"
+#include <time.h>
 int board[ROW][COLUMN] = {{0,0,0,0,0,0,0,0,0},
                           {0,0,0,0,0,0,0,0,0},
-                          {0,1,0,0,0,0,6,0,0},
                           {0,0,0,0,0,0,0,0,0},
                           {0,0,0,0,0,0,0,0,0},
-                          {0,0,0,0,5,0,0,0,0},
                           {0,0,0,0,0,0,0,0,0},
                           {0,0,0,0,0,0,0,0,0},
-                          {0,0,0,0,0,0,0,4,0},};
+                          {0,0,0,0,0,0,0,0,0},
+                          {0,0,0,0,0,0,0,0,0},
+                          {0,0,0,0,0,0,0,0,0},};
 int main()
 {
+    clock_t start_time, end_time;
+    double cpu_time_used;
+    start_time = clock();
+
     sudoku_helper(board, 0, 0);
+    end_time = clock();
+    cpu_time_used = ((double) (end_time - start_time)) / CLOCKS_PER_SEC;
+    printf("CPU time used: %f seconds\n", cpu_time_used);
+
     display(board);
     return 0;
 }
